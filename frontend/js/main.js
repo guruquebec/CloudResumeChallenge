e1b5593dbf0790996d9ec51d4e93e6386958f690
guruquebec <guruquebec@yahoo.ca>
2024-11-24 15:29:00 -0500

    added workflow, frontend and backend files and set up front end files

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -6,6 +6,8 @@ window.addEventListener('DOMContentLoaded',(event)=> {
 })
 
 
+const functionapiURL = 'https://httpsecond.azurewebsites.net/api/httpsecond?code=wS4qH5hUehhw-ZwwexF3ifR2DemaLn7Jtnj86QTRIG20AzFukV73WA%3D%3D'
+const functionApi = 'http://localhost:7071/api/httpsecond';
 
 const getVisitCount = () =>{
     let count = 20;
